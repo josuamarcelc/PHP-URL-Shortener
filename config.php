524e9e0451e7f4a071b1e0f54006120c6ba34c32
root <root@alternatif.website>
2022-01-08 08:58:13 +0000

    initial commit

--- config.php
+++ config.php
@@ -1,14 +1,16 @@
 <?php
 /*
  * First authored by Brian Cray
+ * Forked and edited by Josua Marcel Chrisano
  * License: http://creativecommons.org/licenses/by/3.0/
  * Contact the author at http://briancray.com/
+ * Editor at https://josuamarcelc.com/
  */
 
 // db options
-define('DB_NAME', 'your db name');
-define('DB_USER', 'your db usernae');
-define('DB_PASSWORD', 'your db password');
+define('DB_NAME', 'x');
+define('DB_USER', 'x');
+define('DB_PASSWORD', 'x');
 define('DB_HOST', 'localhost');
 define('DB_TABLE', 'shortenedurls');
 
@@ -17,7 +19,7 @@ mysql_connect(DB_HOST, DB_USER, DB_PASSWORD);
 mysql_select_db(DB_NAME);
 
 // base location of script (include trailing slash)
-define('BASE_HREF', 'http://' . $_SERVER['HTTP_HOST'] . '/');
+define('BASE_HREF', 'https://' . $_SERVER['HTTP_HOST'] . '/');
 
 // change to limit short url creation to a single IP
 define('LIMIT_TO_IP', $_SERVER['REMOTE_ADDR']);
